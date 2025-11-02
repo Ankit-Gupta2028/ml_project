fbdd198664e5206c3383960f7c3a6f7502805063
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2025-11-02 17:14:16 +0530

    loggind and exception

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os 
+from datetime import datetime
+
+LOF_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOF_FILE)
+os.makedirs(logs_path,exist_ok=True) #os.makedirs(logs_path,exist_ok=True) Because your logs (messages about what your program is doing) need a folder to be saved inside.
+
+LOG_FILE_PATH=os.path.join(logs_path,LOF_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
